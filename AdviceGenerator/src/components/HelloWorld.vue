4b9fa8f930a7f68e8332ed243aa0f72b20d2ebeb
Brayan Garzon <destroyergarzon@gmail.com>
2023-11-05 22:18:18 -0500

    Generetor Advince Finish

--- AdviceGenerator/src/components/HelloWorld.vue
+++ AdviceGenerator/src/components/HelloWorld.vue
@@ -1,44 +1,184 @@
-<script setup>
-defineProps({
-  msg: {
-    type: String,
-    required: true
+<script>
+import axios from 'axios';
+
+
+export default {
+  data() {
+  return {
+    advice: null,
+  };
+},
+
+  methods: {
+    async getAdvice() {
+  try {
+    const response = await axios.get('https://api.adviceslip.com/advice');
+    console.log('Datos de la API:', response.data);
+
+    if (response.data && response.data.slip) {
+      this.advice = response.data.slip;
+    } else {
+      console.error('Datos inesperados en la respuesta de la API');
+    }
+  } catch (error) {
+    console.error('Error al obtener el consejo', error);
   }
-})
+}
+
+
+}
+
+
+};
+
 </script>
 
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3>
-  </div>
+
+
+  <section>
+    <div class="card">
+      <div v-if="advice" class="tittle"><h4>ADVICE #{{ advice.id }}</h4></div>
+
+      
+
+      <div v-if="advice" class="text">{{ advice.advice }}</div>
+      
+      <div class="lineas">
+        <hr id="separadorLinea">
+        <i class="fas fa-circle" id="separador"></i>
+        <hr id="separadorLinea">
+      </div>
+      
+      <button @click="getAdvice" class="floating-button"><i class="fa-solid fa-dice-five"></i></button>
+
+      
+    </div>
+
+  </section>
+  
 </template>
 
 <style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
-}
 
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
+  section{
+    width: 100%;
+    height: 100%;
   }
+
+  section .card{
+    width: 550px;
+    height: 350px;
+    background-color: var(--color-Dark-Grayish-Blue);
+    border-radius: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    padding: 30px; /* Añade el relleno que desees aquí */
+    box-sizing: border-box; /* Mantén el tamaño total del elemento */
+    gap: 5%;
+    position: relative;
+  }
+
+
+  /*styles of the tittle*/
+
+  section .card .tittle{
+    width: 100%;
+    height: 5%;
+    text-align: center;
+    color: var(--color-neon-green);
+  }
+  section .card .tittle h4{
+    margin: 0;
+    padding: 0;
+    font-size: 14px;
+    font-family: var(--font-family);
+    letter-spacing: 4px;
+  }
+
+  /*styles of the text*/
+  section .card .text{
+    width: 100%;
+    height: 70%;
+    text-align: center;
+    font-size: var(--font-size);
+    font-weight: var(--font-wights);
+    font-family: var(--font-family);
+    color: var(--color-text);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  /*styles of the lineas*/
+  section .card .lineas{
+    width: 100%;
+    height: calc(100% - 90%);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: row;
+    gap: 10px;
+  }
+  section .card .lineas p{
+    margin: 0;
+    padding: 0;
+  }
+  section .card .lineas hr{
+    width: 100%;
+    color: var(--color-text);
+    margin: 0;
+    padding: 0;
+  }
+
+  section .card .lineas #separador{
+    color: var(--color-text);
+  }
+
+
+  /*styles of the button*/
+  section .card button{
+    height: 50px;
+    width: 50px;
+    border-radius: 50%;
+    border: transparent;
+    background-color: var(--color-neon-green);
+    cursor: pointer;
+  }
+  section .card button i{
+    font-size: 30px;
+  }
+  /* Estilos para la sombra cuando el puntero pasa sobre el botón */
+  section .card button:hover {
+    box-shadow: 0 0 25px hsl(150, 100%, 66%); /* Cambia el color de la sombra aquí */
+  }
+
+  .floating-button {
+    position: absolute;
+    bottom: -25px; /* Ajusta la posición vertical según tu preferencia */
+    z-index: 1; /* Asegura que esté por encima del "card" */
+    /* Estilos adicionales según tus preferencias */
+  }
+
+  
+
+
+
+  
+
+
+/* Estilos específicos para pantallas pequeñas (Mobile: 375px o menor) */
+@media screen and (max-width: 414px) {
+  section .card {
+    width: 370px; /* Ajusta el ancho para pantallas pequeñas */
+    /* Otros estilos específicos para pantallas pequeñas */
+  }
+
+  section .card .tittle h4 {
+    font-size: 12px; /* Ajusta el tamaño de fuente para pantallas pequeñas */
+    /* Otros estilos específicos para pantallas pequeñas */
+  }
+  /* Otros estilos específicos para pantallas pequeñas */
 }
 </style>
---
+++ Readme.md
@@ -0,0 +1,38 @@
+# Generador de Consejos
+
+Este es un generador de consejos simple desarrollado en Vue.js que utiliza la API de Advice Slip para proporcionar consejos aleatorios. Puedes usar este generador para obtener consejos inspiradores o divertidos en cualquier momento.
+
+
+## Características
+
+- Generación de consejos aleatorios con un solo clic.
+- Diseño atractivo y responsivo.
+- Integración con la API de Advice Slip para obtener consejos en tiempo real.
+
+## Capturas de Pantalla
+
+![Captura de Pantalla 1]('/AdviceGenerator/src/assets/design/active-states.jpg')
+![Captura de Pantalla 2](/AdviceGenerator/src/assets/design/mobile-design.jpg)
+
+## Uso
+
+1. Clona o descarga este repositorio a tu computadora.
+2. Abre el proyecto en tu editor de código.
+3. Instala las dependencias ejecutando `npm install`.
+4. Inicia la aplicación con `npm run dev`.
+5. Accede a la aplicación en tu navegador en `http://localhost:8080`.
+
+## Personalización
+
+Si deseas personalizar la apariencia o el comportamiento del generador de consejos, puedes hacerlo modificando los archivos del proyecto. Puedes ajustar los estilos en los archivos CSS y modificar la lógica en los componentes Vue.
+
+## Tecnologías Utilizadas
+
+- Vue.js
+- HTML
+- CSS
+
+## Contribuciones
+
+Si deseas contribuir a este proyecto, ¡eres bienvenido! Siéntete libre de crear un Pull Request o informar sobre problemas.
+
